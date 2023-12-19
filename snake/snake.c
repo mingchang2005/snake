d221edd16f01be58be8f205bd939c75e5d422d5e
立名 張 <tw.ming.chang@gmail.com>
2023-12-19 10:12:55 +0800

    1

--- snake/snake.c
+++ snake/snake.c
@@ -58,7 +58,7 @@ void initializeFood() {
 // 在控制台上繪製蛇和食物以及分數
 void drawSnakeAndFood() {
     gotoxy(food.x, food.y);
-    printf("★");  // 繪製食物
+    printf("*");  // 繪製食物
     for (int i = 0; i < snake.length; i++) {
         gotoxy(snake.body[i].x, snake.body[i].y);
         if (i == 0)
@@ -67,7 +67,7 @@ void drawSnakeAndFood() {
             printf("@");  // 繪製蛇的身體
     }
     gotoxy(WIDTH + 5, 1);
-    printf("第%d回合得分: %d(按下x可結束)\0", g+1, snake.length);
+    printf("第%d回合得分: %d(按下x可結束)\0", g + 1, snake.length);
 }
 // 根據當前方向移動蛇
 void moveSnake() {
@@ -91,10 +91,12 @@ void moveSnake() {
 }
 // 檢查是否與牆壁或自己碰撞的函數
 int checkCollision() {
-    if (snake.body[0].x <= 0 || snake.body[0].x > WIDTH ||
-        snake.body[0].y <= 0 || snake.body[0].y > HEIGHT) {
+    if (snake.body[0].x <= 0 || snake.body[0].x > WIDTH || snake.body[0].y <= 0 || snake.body[0].y > HEIGHT) {
         return 1;  // 與牆壁碰撞
     }
+    // 清理碰撞牆壁的尾巴部分
+    gotoxy(snake.body[snake.length - 1].x, snake.body[snake.length - 1].y);
+    printf(" ");
     for (int i = 1; i < snake.length; i++) {
         if (snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y) {
             return 1;  // 與自己碰撞
@@ -110,48 +112,53 @@ void checkFood() {
     }
 }
 int main() {
-    do
-    {
-        for (g = 0; g < 3; g++)
-        {
+    // Clear the screen once before the game starts
+    system("cls");
+    do {
+        for (g = 0; g < 3; g++) {
+            system("cls");
             initializeSnake();
             initializeFood();
+            drawMap();  // Redraw the map once at the beginning of each game
             while (1) {
                 if (_kbhit()) {
                     char ch = _getch();
                     switch (ch) {
                     case 'w':
-                        if (snake.direction != 2 || snake.length == 1)  // 不允許向下移動時按上，長度為1時可隨意移動
+                        if (snake.direction != 2 || snake.length == 1)
                             snake.direction = 1;
                         break;
                     case 's':
-                        if (snake.direction != 1 || snake.length == 1)  // 不允許向上移動時按下，長度為1時可隨意移動
+                        if (snake.direction != 1 || snake.length == 1)
                             snake.direction = 2;
                         break;
                     case 'a':
-                        if (snake.direction != 4 || snake.length == 1)  // 不允許向右移動時按左，長度為1時可隨意移動
+                        if (snake.direction != 4 || snake.length == 1)
                             snake.direction = 3;
                         break;
                     case 'd':
-                        if (snake.direction != 3 || snake.length == 1)  // 不允許向左移動時按右，長度為1時可隨意移動
+                        if (snake.direction != 3 || snake.length == 1)
                             snake.direction = 4;
                         break;
                     case 'z':
-                    {
                         drawSnakeAndFood();
                         printf("\n█");
                         system("pause");
-                    }
-                    break;
+                        break;
                     case 'x':
-                    {
                         system("cls");
                         exit(0);
-                    }
-                    break;
+                        break;
                     }
                 }
                 moveSnake();
+                // Instead of clearing the entire screen, update only the necessary parts
+                drawSnakeAndFood();
+                checkFood();
+                Sleep(SPEED);
+                // Clear the area where the snake was to avoid artifacts
+                gotoxy(snake.body[snake.length - 1].x, snake.body[snake.length - 1].y);
+                printf(" ");
                 if (snake.length == MAX_LENGTH) {
                     ro = g + 1;
                     g = life;
@@ -163,11 +170,6 @@ int main() {
                     win = 0;
                     break;
                 }
-                drawSnakeAndFood();
-                checkFood();
-                Sleep(SPEED);
-                system("cls");
-                drawMap();
             }
         }
         Sleep(SPEED);
@@ -179,7 +181,9 @@ int main() {
             printf("You Win!\n得分:%d\n使用%d回合\n", MAX_LENGTH * life, ro);
         else
             printf("\nGame Over!\n總得分:%d\n", tt);
+        tt = 0;
+        // After each game, ask the user to start a new game or exit
         printf("按下任意鍵開始新遊戲或按下x結束\n");
-        _getch();//避免使用者輸入過快跳過結算畫面
-    }while (_getch() != 'x');
+    } while (_getch() != 'x');
+    return 0;
 }
