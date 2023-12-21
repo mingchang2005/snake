691f8e1811405abcefbd79570fe86d6523259746
mingchang2005 <150763136+mingchang2005@users.noreply.github.com>
2023-12-21 11:46:38 +0800

    Delete snake directory

--- snake/parameter.h
+++
@@ -1,8 +0,0 @@
-#define WIDTH 30
-#define HEIGHT 20
-#define DEFAULT_MAX_LENGTH 5
-#define SPEED 100
-#define life 3
-#define MAXpoint 50
-int g, tt = 0, ro, win,rod=1;
-int MAX_LENGTH = DEFAULT_MAX_LENGTH;  // 最大長度
