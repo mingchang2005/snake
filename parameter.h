82a233d4708d3521dcb7d20a19b41ed0e8d707a8
mingchang2005 <150763136+mingchang2005@users.noreply.github.com>
2023-12-21 11:45:13 +0800

    all file
    all file snake

---
+++ parameter.h
@@ -0,0 +1,25 @@
+#define WIDTH 30
+#define HEIGHT 20
+#define DEFAULT_MAX_LENGTH 5
+#define SPEED 100
+#define life 3
+#define MAXpoint 50
+void gotoxy(int, int);
+void drawMap();
+void initializeSnake();
+void moveSnake();
+void initializeFood();
+void initializeGame();
+int checkCollision();
+int checkFood();
+typedef struct {
+    int x;
+    int y;
+} Point;
+typedef struct {
+    int length;
+    Point body[MAXpoint];
+    int direction;  // 1: ¤W, 2: ¤U, 3: ¥ª, 4: ¥k
+} Snake;
+Point food;
+Snake snake;
